eff0992826f33f4eaebfc818dc2e55139c101194
Dobee-Kim <dorosy555@naver.com>
2025-02-04 16:18:36 +0900

    feat : NavBar, 레이아웃 업데이트 S12P11A707-171 #done

--- frontend/ttabong/src/App.tsx
+++ frontend/ttabong/src/App.tsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from "@/hooks/theme-provider";
+import { MainLayout } from "@/layout/MainLayout";
+import { PageLayout } from "@/layout/PageLayout";
+
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="min-h-screen bg-zinc-100 dark:bg-zinc-900">
+        <div className="mx-auto max-w-[600px] min-w-[320px] h-screen bg-background">
+          <MainLayout>
+            <PageLayout>
+              <Routes>
+
+              </Routes>
+            </PageLayout>
+          </MainLayout>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
