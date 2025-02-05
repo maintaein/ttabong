bd91181305beaaa4a19cffce8ece63edce9ee7ed
recreate.kang <recreate.kang@gmail.com>
2025-02-05 18:50:41 +0900

    feat: recruit domain entity

---
+++ backend/ttabong/src/main/java/com/ttabong/entity/recruit/VolunteerReaction.java
@@ -0,0 +1,40 @@
+package com.ttabong.entity.recruit;
+
+import com.ttabong.entity.user.Volunteer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Volunteer_reaction")
+public class VolunteerReaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reaction_id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "volunteer_id")
+    private Volunteer volunteer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Recruit_id")
+    private Recruit recruit;
+
+    @Column(name = "is_like", nullable = false)
+    private Boolean isLike = false;
+
+    @ColumnDefault("0")
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+}
