b0777fc836304a988fdc523afb0763aae915954c
recreate.kang <recreate.kang@gmail.com>
2025-02-07 06:18:59 +0900

    feat: controller mapped with proper dto response, request

--- backend/ttabong/src/main/java/com/ttabong/controller/recruit/OrgRecruitController.java
+++ backend/ttabong/src/main/java/com/ttabong/controller/recruit/OrgRecruitController.java
@@ -1,9 +1,13 @@
 package com.ttabong.controller.recruit;
 
 import com.ttabong.dto.recruit.requestDto.org.*;
+import com.ttabong.dto.recruit.responseDto.vol.ReadMyRecruitsResponseDto;
+import com.ttabong.dto.recruit.responseDto.org.ReadAvailableRecruitsResponseDto;
 import com.ttabong.dto.recruit.responseDto.org.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("org")
@@ -11,37 +15,37 @@ public class OrgRecruitController {
 
     //1
     @GetMapping("/templates/available")
-    public ResponseEntity<?> readAvailableRecruits(@RequestParam Integer cursor, @RequestParam Integer limit) {
+    public ResponseEntity<ReadAvailableRecruitsResponseDto> readAvailableRecruits(@RequestParam Integer cursor, @RequestParam Integer limit) {
         return ResponseEntity.ok().build();
     }
 
     //2
     @GetMapping("/recruits")
-    public ResponseEntity<?> readRecruits(@RequestParam(required = true) int cursor, @RequestParam(defaultValue = "10") int limit) {
+    public ResponseEntity<ReadMyRecruitsResponseDto> readRecruits(@RequestParam(required = true) int cursor, @RequestParam(defaultValue = "10") int limit) {
         return ResponseEntity.ok().build();
     }
 
     //3 b
     @PatchMapping("recruits/delete")
-    public ResponseEntity<DeleteRecruitsResponse> deleteRecruits(@RequestBody DeleteRecruitsRequestDto deleteRecruitDto) {
+    public ResponseEntity<DeleteRecruitsResponseDto> deleteRecruits(@RequestBody DeleteRecruitsRequestDto deleteRecruitDto) {
         return ResponseEntity.ok().build();
     }
 
     //4 b
     @PatchMapping("recruits/{recruitId}")
-    public ResponseEntity<UpdateRecruitsResponse> updateRecruit(@PathVariable int recruitId, @RequestBody UpdateRecruitsRequestDto updateRecruitDto) {
+    public ResponseEntity<UpdateRecruitsResponseDto> updateRecruit(@PathVariable int recruitId, @RequestBody UpdateRecruitsRequestDto updateRecruitDto) {
         return ResponseEntity.ok().build();
     }
 
     //5 b
     @PatchMapping("recruits/close")
-    public ResponseEntity<CloseRecruitResponse> closeRecruit(@RequestBody CloseRecruitRequestDto closeRecruitDto) {
+    public ResponseEntity<CloseRecruitResponseDto> closeRecruit(@RequestBody CloseRecruitRequestDto closeRecruitDto) {
         return ResponseEntity.ok().build();
     }
 
     //7 6
     @PatchMapping("groups")
-    public ResponseEntity<UpdateGroupResponse> updateGroup(@RequestBody UpdateGroupRequestDto updateGroupDto) {
+    public ResponseEntity<UpdateGroupResponseDto> updateGroup(@RequestBody UpdateGroupRequestDto updateGroupDto) {
         return ResponseEntity.ok().build();
     }
 
@@ -53,61 +57,61 @@ public class OrgRecruitController {
 
     //8 b
     @PatchMapping("templates/delete")
-    public ResponseEntity<DeleteTemplatesResponse> deleteTemplates(@RequestBody DeleteTemplatesRequestDto deleteTemplatesDto) {
+    public ResponseEntity<DeleteTemplatesResponseDto> deleteTemplates(@RequestBody DeleteTemplatesRequestDto deleteTemplatesDto) {
         return ResponseEntity.ok().build();
     }
 
     //9 b
     @PatchMapping("groups/delete")
-    public ResponseEntity<DeleteGroupResponse> deleteGroup(@RequestBody DeleteGroupDto deleteGroupDto) {//@RequestBody Object object) {
+    public ResponseEntity<DeleteGroupResponseDto> deleteGroup(@RequestBody DeleteGroupDto deleteGroupDto) {//@RequestBody Object object) {
         return ResponseEntity.ok().build();
     }
 
     //10
     @GetMapping("templates")
-    public ResponseEntity<?> readTemplates(@RequestParam(defaultValue = "0") int cursor, @RequestParam(defaultValue = "10") int limit) {
+    public ResponseEntity<ReadTemplatesResponseDto> readTemplates(@RequestParam(defaultValue = "0") int cursor, @RequestParam(defaultValue = "10") int limit) {
         return ResponseEntity.ok().build();
     }
 
     //11 b
     @PostMapping("templates")
-    public ResponseEntity<CreateTemplateResponse> createTemplate(@RequestBody CreateTemplateRequestDto createTemplateDto) {//@RequestBody Object object) {
+    public ResponseEntity<CreateTemplateResponseDto> createTemplate(@RequestBody CreateTemplateRequestDto createTemplateDto) {//@RequestBody Object object) {
         return ResponseEntity.ok().build();
     }
 
     //12 b
     @PostMapping("groups")
-    public ResponseEntity<CreateGroupResponse> createGroup(@RequestBody CreateGroupRequestDto createGroupDto) {//@RequestBody Object object) {
+    public ResponseEntity<CreateGroupResponseDto> createGroup(@RequestBody CreateGroupRequestDto createGroupDto) {//@RequestBody Object object) {
         return ResponseEntity.ok().build();
     }
 
     //13 b
     @PostMapping("recruits")
-    public ResponseEntity<CreateRecruitResponse> createRecruit(@RequestBody CreateRecruitRequestDto createRecruitDto) {//@RequestBody Object object) {
+    public ResponseEntity<CreateRecruitResponseDto> createRecruit(@RequestBody CreateRecruitRequestDto createRecruitDto) {//@RequestBody Object object) {
         return ResponseEntity.ok().build();
     }
 
     //14
     @GetMapping("recruits/{recruitId}")
-    public ResponseEntity<?> readRecruit() {//@PathVariable int recruitId) {
+    public ResponseEntity<readRecruitResponseDto> readRecruit() {//@PathVariable int recruitId) {
         return ResponseEntity.ok().build();
     }
 
     //15
     @GetMapping("recruits/{recruitId}/applications")
-    public ResponseEntity<?> readApplications(@PathVariable int recruitId) {
+    public ResponseEntity<readApplicationsResponseDto> readApplications(@PathVariable int recruitId) {
         return ResponseEntity.ok().build();
     }
 
     //16 b
     @PatchMapping("applications/status")
-    public ResponseEntity<UpdateApplicationsResponse> updateStatuses(@RequestBody UpdateApplicationsRequestDto updateApplicationDto) {//@RequestBody Object object) {
+    public ResponseEntity<UpdateApplicationsResponseDto> updateStatuses(@RequestBody UpdateApplicationsRequestDto updateApplicationDto) {//@RequestBody Object object) {
         return ResponseEntity.ok().build();
     }
 
     //17 b
     @PatchMapping("recruits/{recruitId}/applications/evaluate")
-    public ResponseEntity<EvaluateApplicationsResponse> evaluateApplicants(@PathVariable int recruitId, EvaluateApplicationsRequestDto evaluateApplicationDto) {//}, @RequestBody Object object) {
+    public ResponseEntity<List<EvaluateApplicationsResponseDto>> evaluateApplicants(@PathVariable int recruitId, EvaluateApplicationsRequestDto evaluateApplicationDto) {//}, @RequestBody Object object) {
         return ResponseEntity.ok().build();
     }
 }
--- backend/ttabong/src/main/java/com/ttabong/controller/recruit/VolRecruitController.java
+++ backend/ttabong/src/main/java/com/ttabong/controller/recruit/VolRecruitController.java
@@ -1,5 +1,10 @@
 package com.ttabong.controller.recruit;
 
+import com.ttabong.dto.recruit.requestDto.vol.likeOnRecruitRequestDto;
+import com.ttabong.dto.recruit.requestDto.vol.ApplyRecruitRequestDto;
+import com.ttabong.dto.recruit.requestDto.vol.deleteLikesRequestDto;
+import com.ttabong.dto.recruit.responseDto.vol.likeOnRecruitResponseDto;
+import com.ttabong.dto.recruit.responseDto.vol.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,48 +13,48 @@ import org.springframework.web.bind.annotation.*;
 public class VolRecruitController {
 
     @GetMapping("templates")
-    public ResponseEntity<?> listRecruits(@RequestParam Integer cursor, @RequestParam Integer limit) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ReadRecruitsResponseDto> listRecruits(@RequestParam Integer cursor, @RequestParam Integer limit) {
+        return ResponseEntity.ok().body(new ReadRecruitsResponseDto());
     }
 
     @GetMapping("templates/{templateId}")
-    public ResponseEntity<?> RecruitsDetail(@PathVariable String templateId) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ReadRecruitDetailResponseDto> RecruitsDetail(@PathVariable String templateId) {
+        return ResponseEntity.ok().body(new ReadRecruitDetailResponseDto());
     }
 
     @PostMapping("applications")
-    public ResponseEntity<?> applyRecruit() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ApplyRecruitResponseDto> applyRecruit(@RequestBody ApplyRecruitRequestDto applyRecruitDto) {
+        return ResponseEntity.ok().body(new ApplyRecruitResponseDto());
     }
 
     @PatchMapping("applications/{applicationsId}")
-    public ResponseEntity<?> cancelRecruit(@PathVariable String applicationsId) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<CancelRecruitResponseDto> cancelRecruit(@PathVariable String applicationsId) {
+        return ResponseEntity.ok().body(new CancelRecruitResponseDto());
     }
 
     @GetMapping("applications/recruits")
-    public ResponseEntity<?> myApplications(@RequestParam Integer cursor, @RequestParam Integer limit) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<MyApplicationsResponseDto> myApplications(@RequestParam Integer cursor, @RequestParam Integer limit) {
+        return ResponseEntity.ok().body(new MyApplicationsResponseDto());
     }
 
-    @GetMapping("recruits")
-    public ResponseEntity<?> myApplicationsDetail(@RequestParam Integer cursor, @RequestParam Integer limit) {
-        return ResponseEntity.ok().build();
+    @GetMapping("recruits/{recruitId}")
+    public ResponseEntity<MyApplicationDetailResponseDto> myApplicationsDetail(@PathVariable String recruitId) {
+        return ResponseEntity.ok().body(new MyApplicationDetailResponseDto());
 
     }
 
     @GetMapping("volunteer-reactions/like")
-    public ResponseEntity<?> myLikesOnRecruits(@RequestParam Integer cursor, @RequestParam Integer limit) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<MyLikesRecruitsResponseDto> myLikesOnRecruits(@RequestParam Integer cursor, @RequestParam Integer limit) {
+        return ResponseEntity.ok().body(new MyLikesRecruitsResponseDto());
     }
 
     @PostMapping("volunteer_reactions")
-    public ResponseEntity<?> likeOnRecruit() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<likeOnRecruitResponseDto> likeOnRecruit(@RequestBody likeOnRecruitRequestDto likeOnRecruitRequest) {
+        return ResponseEntity.ok().body(new likeOnRecruitResponseDto());
     }
 
     @PatchMapping("volunteer_reactions/cancel")
-    public ResponseEntity<?> deleteRecruitFromLike(@RequestParam Integer cursor, @RequestParam Integer limit) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> deleteRecruitFromLike(@RequestBody deleteLikesRequestDto deleteLikeRequest) {
+        return ResponseEntity.noContent().build();
     }
 }
