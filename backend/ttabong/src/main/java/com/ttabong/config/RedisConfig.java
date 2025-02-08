89c6a9ae7fa9e7e2d6d3d90f27b4c534acad9f97
recreate.kang <recreate.kang@gmail.com>
2025-02-08 20:48:49 +0900

    feat : Redis Additional Setting done

--- backend/ttabong/src/main/java/com/ttabong/config/RedisConfig.java
+++ backend/ttabong/src/main/java/com/ttabong/config/RedisConfig.java
@@ -1,11 +1,16 @@
 package com.ttabong.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -24,7 +29,27 @@ public class RedisConfig {
     RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(lettuceConnectionFactory());
-        //template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(KeyWord.class));
+
+        // 새로운 Jackson2JsonRedisSerializer 사용 방식
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        // Key & Value Serializer 설정
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        return template;
+    }
+
+    @Bean
+    RedisTemplate<String, Long> redisKeyTemplate() {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory());
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer()); // Redis의 값도 String으로 저장됨
+
         return template;
     }
 }
