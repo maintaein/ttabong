fe6ba212ee48a4a5995878265be73b27771dc985
maintaein <daumdaum005@gmail.com>
2025-02-10 16:04:34 +0900

    feat: 기본리액트설정

--- frontend/ttabong/src/App.tsx
+++ frontend/ttabong/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from "@/hooks/theme-provider";
 import { MainLayout } from "@/layout/MainLayout";
