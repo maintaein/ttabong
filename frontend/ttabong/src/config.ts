64fb745dbf81abd2c51640298468c6d6ef248a88
Dobee-Kim <dorosy555@naver.com>
2025-02-20 13:52:25 +0900

    chore : http로 다운그레이드

--- frontend/ttabong/src/config.ts
+++ frontend/ttabong/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: import.meta.env.VITE_API_URL || 'https://ttabong.store' || 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL || 'http://ttabong.store' || 'http://localhost:8080',
   apiPrefix: '/api',
   timeout: 5000,
   rules: {
