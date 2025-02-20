c8957d63c96b7f3c92be92d841ff4d2ef3aebc0a
박유진 <galilee155@naver.com>
2025-02-20 10:05:23 +0900

    fix: read review detail

--- backend/ttabong/src/main/java/com/ttabong/repository/sns/ReviewCommentRepository.java
+++ backend/ttabong/src/main/java/com/ttabong/repository/sns/ReviewCommentRepository.java
@@ -4,10 +4,11 @@ import com.ttabong.entity.sns.ReviewComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Integer> {
     Optional<ReviewComment> findByIdAndIsDeletedFalse(Integer commentId);
-    Optional<ReviewComment> findByReviewIdAndIsDeletedFalseOrderByCreatedAt(Integer reviewId);
+    List<ReviewComment> findByReviewIdAndIsDeletedFalseOrderByCreatedAt(Integer reviewId);
 }
